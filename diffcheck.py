703ced5e2dc6f18d97252166f5fbbc5e8ed620db
IshikaDe-2803 <81436870+IshikaDe-2803@users.noreply.github.com>
2023-09-23 21:18:04 +0530

    Update diffcheck.py

--- diffcheck.py
+++ diffcheck.py
@@ -1,18 +1,5 @@
 
 
-api_params = {
-    'token': api_key,
-    # 'url': 'https://github.com/IshikaDe-2803/MiniProject_SemIV',  # Leave this empty for raw text analysis
-    'html': document_content,  # Use this for raw text analysis
-}
-
-# Send a POST request to the Diffbot API
-response = requests.post(diffbot_api_url, data=api_params)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON response
-    data = json.loads(response.text)
     
     # Extract the plagiarism score (Diffbot provides a similarity score)
     plagiarism_score = data.get('plagiarismScore', None)
