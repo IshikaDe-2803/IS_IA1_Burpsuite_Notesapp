761b090d5ae3d906fe200b43c7557831fed3502a
IshikaDe-2803 <81436870+IshikaDe-2803@users.noreply.github.com>
2023-09-23 21:20:03 +0530

    Update diffcheck.py

--- diffcheck.py
+++ diffcheck.py
@@ -1,9 +1,6 @@
 
 
     
-    # Extract the plagiarism score (Diffbot provides a similarity score)
-    plagiarism_score = data.get('plagiarismScore', None)
-    
     if plagiarism_score is not None:
         print(f"Plagiarism Score: {plagiarism_score}")
     else:
