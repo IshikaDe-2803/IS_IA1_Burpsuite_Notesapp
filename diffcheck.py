cc6cc1b2ef3242a49a35b2c85f02f36e3eb964d7
IshikaDe-2803 <81436870+IshikaDe-2803@users.noreply.github.com>
2023-09-23 20:19:03 +0530

    Update diffcheck.py now at 20:18

--- diffcheck.py
+++ diffcheck.py
@@ -1,10 +1,4 @@
-Abstract:
-After the new Coronavirus disease (COVID-19) case spread rapidly in Wuhan-China in December 2019, World Health Organization (WHO) confirmed that this is a dangerous virus that can be spread from humans to humans through droplets and airborne. As for prevention, wearing a face mask is essential while going outside or meeting with others. Moreover, developing the face mask detector is very crucial in this case.
 
-Scope:
-The idea is to create a mask detection system that can be employed at different organizations and can fit into door opening systems to check whether employees/clients/customers are wearing masks to prevent deadly viruses.
-
-Aim:
 To create a website that will check if the person is wearing a mask or not.
 
 Target Audience:
