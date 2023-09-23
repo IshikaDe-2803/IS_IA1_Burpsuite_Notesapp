ba9b08f51722f598517d6edf12815a3366119408
IshikaDe-2803 <81436870+IshikaDe-2803@users.noreply.github.com>
2023-09-23 20:21:50 +0530

    Update diffcheck.py at 20:21

--- diffcheck.py
+++ diffcheck.py
@@ -1,22 +1,5 @@
 
-To create a website that will check if the person is wearing a mask or not.
 
-Target Audience:
-This web application is aimed at helping organizations take precautions against covid 19 by tracking whether employees are wearing masks.
-
-Tech Stack:
-FrontEnd: Figma, HTML, CSS, Bootstrap BackEnd: Flask Libraries: OpenCV, Keras, tensorflow
-
-References:
-
-Rapid Object Detection using a Boosted Cascade of Simple Features - Viola and Jones
-https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html
-"""
-
-# Define the Diffbot API endpoint for article analysis
-diffbot_api_url = 'https://api.diffbot.com/v3/article'
-
-# Set up the API request parameters
 api_params = {
     'token': api_key,
     # 'url': 'https://github.com/IshikaDe-2803/MiniProject_SemIV',  # Leave this empty for raw text analysis
